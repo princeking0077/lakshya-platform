cdfb488df176b6c673f10dc6270a03b114426046
shoai <shoai@hostel.com>
2026-01-01 02:14:35 +0530

    Test: Switch to simple test server

---
+++ client/test_server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from Node.js Server - Control Verified');
+});
+
+server.listen(port, () => {
+    console.log(`Test server running at port ${port}`);
+});
